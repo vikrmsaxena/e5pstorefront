dc5cc80c480af166d9bd004c6f33d793997e993c
amitkumar1403 <91612827+amitkumar1403@users.noreply.github.com>
2021-11-30 19:24:54 +0530

    Update Pagination UI and alignment

--- components/product/Pagination/index.tsx
+++ components/product/Pagination/index.tsx
@@ -16,8 +16,8 @@ const Pagination: FC<Props> = ({
     <>
       <ReactPaginate
         previousLabel={'Previous'}
-        previousClassName="py-1 mr-40 inline-flex items-center px-4 h-10 border border-gray-300 rounded-md bg-white hover:bg-gray-100 focus:outline-none focus:border-indigo-600 focus:ring-2 focus:ring-offset-1 focus:ring-offset-indigo-600 focus:ring-indigo-600 focus:ring-opacity-25"
-        nextClassName="py-1 ml-40 inline-flex items-center px-4 h-10 border border-gray-300 rounded-md bg-white hover:bg-gray-100 focus:outline-none focus:border-indigo-600 focus:ring-2 focus:ring-offset-1 focus:ring-offset-indigo-600 focus:ring-indigo-600 focus:ring-opacity-25"
+        previousClassName="py-1 mr-4 inline-flex items-center px-4 h-10 border border-gray-300 rounded-md bg-white hover:bg-gray-100 focus:outline-none focus:border-indigo-600 focus:ring-2 focus:ring-offset-1 focus:ring-offset-indigo-600 focus:ring-indigo-600 focus:ring-opacity-25"
+        nextClassName="py-1 ml-4 inline-flex items-center px-4 h-10 border border-gray-300 rounded-md bg-white hover:bg-gray-100 focus:outline-none focus:border-indigo-600 focus:ring-2 focus:ring-offset-1 focus:ring-offset-indigo-600 focus:ring-indigo-600 focus:ring-opacity-25"
         nextLabel={'Next'}
         breakLabel={'...'}
         breakClassName={'inline-flex items-center text-gray-500 px-1.5 h-10'}
@@ -26,14 +26,14 @@ const Pagination: FC<Props> = ({
         pageRangeDisplayed={5}
         initialPage={Number(currentPage) - 1}
         disableInitialCallback={true}
-        pageClassName="px-2 py-1"
+        pageClassName="px-2 py-0"
         activeLinkClassName="inline-flex items-center px-4 h-10 border border-indigo-600 ring-1 ring-indigo-600 rounded-md bg-white hover:bg-gray-100 focus:outline-none focus:border-indigo-600 focus:ring-2 focus:ring-offset-1 focus:ring-offset-indigo-600 focus:ring-indigo-600 focus:ring-opacity-25"
         pageLinkClassName={
           'inline-flex items-center px-4 h-10 border border-gray-300 rounded-md bg-white hover:bg-gray-100 focus:outline-none focus:border-indigo-600 focus:ring-2 focus:ring-offset-1 focus:ring-offset-indigo-600 focus:ring-indigo-600 focus:ring-opacity-25'
         }
         onPageChange={onPageChange}
         containerClassName={
-          'max-w-7xl mx-auto px-4 mt-6 flex justify-between text-sm font-medium text-gray-700 sm:px-6 lg:px-8'
+          'max-w-7xl mx-auto px-0 mt-6 mb-3 flex justify-center text-sm font-medium text-gray-700 sm:px-6 lg:px-0'
         }
       />
     </>
