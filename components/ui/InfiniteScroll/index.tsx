1c564ac582f9f3247eaf90a036cec2a19a4368f1
Adrian Mustea <mustea.adrian95@gmail.com>
2022-02-16 12:29:57 +0200

    wip

--- components/ui/InfiniteScroll/index.tsx
+++ components/ui/InfiniteScroll/index.tsx
@@ -11,7 +11,7 @@ export default function InfiniteScrollComponent({
       dataLength={total} //This is important field to render the next data
       next={fetchData}
       hasMore={currentNumber < total}
-      loader={<h4>Loading...</h4>}
+      loader={<h4 className="text-gray-900">Loading...</h4>}
       endMessage={
         <p style={{ textAlign: 'center' }}>
           <b>You have seen it all</b>
