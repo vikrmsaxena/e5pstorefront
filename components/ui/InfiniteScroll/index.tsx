b8fdee31bd17cbd32f5870028a7b97712abfe591
Adrian Mustea <mustea.adrian95@gmail.com>
2022-02-16 12:57:44 +0200

    loader plp

--- components/ui/InfiniteScroll/index.tsx
+++ components/ui/InfiniteScroll/index.tsx
@@ -1,4 +1,6 @@
 import InfiniteScroll from 'react-infinite-scroll-component'
+import { useState, useEffect } from 'react'
+import LoadingDots from '@components/ui/LoadingDots'
 
 export default function InfiniteScrollComponent({
   component,
@@ -6,12 +8,22 @@ export default function InfiniteScrollComponent({
   fetchData,
   currentNumber,
 }: any) {
+  const [isLoading, setIsLoading] = useState(false)
+  const handleFetch = (...args: any) => {
+    setIsLoading(true)
+    fetchData(...args)
+  }
+
+  useEffect(() => {
+    if (isLoading) setIsLoading(false)
+  }, [currentNumber])
+
   return (
     <InfiniteScroll
       dataLength={total} //This is important field to render the next data
-      next={fetchData}
+      next={handleFetch}
       hasMore={currentNumber < total}
-      loader={<h4 className="text-gray-900">Loading...</h4>}
+      loader={null}
       endMessage={
         <p style={{ textAlign: 'center' }}>
           <b>You have seen it all</b>
@@ -19,6 +31,11 @@ export default function InfiniteScrollComponent({
       }
     >
       {component}
+      {isLoading && (
+        <div className="justify-center items-center flex mt-4 w-full text-black">
+          <LoadingDots />
+        </div>
+      )}
     </InfiniteScroll>
   )
 }
