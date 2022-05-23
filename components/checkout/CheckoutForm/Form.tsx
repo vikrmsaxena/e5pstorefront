f28239a34137c16c289e7df89fdf8cb2c1ab7428
Amit Kumar <amit.kumar@bettercommerce.io>
2022-05-23 17:59:10 +0530

    Update Form.tsx

--- components/checkout/CheckoutForm/Form.tsx
+++ components/checkout/CheckoutForm/Form.tsx
@@ -338,7 +338,7 @@ export default function AddressForm({
               <button
                 type="submit"
                 onClick={(...args) => handleFormSubmit(handleSubmit, ...args)}
-                className="max-w-xs flex-1 bg-black border border-transparent rounded-sm py-3 px-8 flex items-center justify-center font-medium text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-50 focus:ring-gray-500 sm:w-full"
+                className="max-w-xs flex-1 bg-black border uppercase border-transparent rounded-sm py-3 px-8 flex items-center justify-center font-medium text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-50 focus:ring-gray-500 sm:w-full"
               >
                 {btnTitle}
               </button>
