5abaabe73db207a09146450ce8e7b12c87181105
Amit Kumar <amit.kumar@bettercommerce.io>
2022-05-23 12:35:30 +0530

    Update Logo Ratio

--- components/ui/Logo/Logo.tsx
+++ components/ui/Logo/Logo.tsx
@@ -3,11 +3,10 @@ import React from "react";
 const Logo = ({ className = '', ...props }) => {
   return (
     <img
-      className="sm:h-8 w-auto"
       src="/e5p_logo.png"
       alt="everything5pounds"
-      width={401}
-      height={37}
+      width={297}
+      height={27}
     />
   );
 }
