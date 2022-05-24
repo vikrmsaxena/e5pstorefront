cf050374232adb7bfaf86226782bd05d2a3d193c
Amit Kumar <amit.kumar@bettercommerce.io>
2022-05-24 12:48:10 +0530

    Fix PDP Main image quality

--- components/product/ProductView/ProductView.tsx
+++ components/product/ProductView/ProductView.tsx
@@ -439,7 +439,7 @@ export default function ProductView({
                         {image.image ? (
                           <div className='image-container'>
                             <Image
-                              src={`${image.image}?h=400&w=300&fm=webp` || IMG_PLACEHOLDER}                             
+                              src={`${image.image}?h=1000&w=600&fm=webp` || IMG_PLACEHOLDER}                             
                               alt={image.name}
                               className="w-full h-full object-center object-cover image"
                               layout='fill'
