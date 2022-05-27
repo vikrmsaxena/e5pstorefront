50bea7bc28d92a4796810bd0e8ff2fcf91462f35
Amit Kumar <amit.kumar@bettercommerce.io>
2022-05-27 19:36:02 +0530

    Add Load more

--- components/product/Grid/index.tsx
+++ components/product/Grid/index.tsx
@@ -2,23 +2,44 @@ import Pagination from '@components/product/Pagination'
 import rangeMap from '@lib/range-map'
 import ProductCard from '@components/product/ProductCard/SearchProductCard'
 import InfiniteScroll from '@components/ui/InfiniteScroll'
-import { TITLE_PRODUCTS } from '@components/utils/textVariables'
+import { GENERAL_LOAD_MORE, TITLE_PRODUCTS } from '@components/utils/textVariables'
+import Button from '@components/ui/IndigoButton'
 
 interface Props {
   products: any
   currentPage: number | string
   handlePageChange?: any
   handleInfiniteScroll: any
+  isLastPage?: boolean
+  isInfiniteScrollEnabled?: boolean
 }
 
 export default function Grid({
   products,
   currentPage,
-  handlePageChange = () => {},
+  handlePageChange = () => { },
   handleInfiniteScroll,
+  isInfiniteScrollEnabled,
+  isLastPage,
 }: Props) {
-  const IS_INFINITE_SCROLL =
-    process.env.NEXT_PUBLIC_ENABLE_INFINITE_SCROLL === 'true'
+
+  // InfiniteScroll flag is now available at parent level.
+  // If parent level flag is not defined, then use the global config setting.
+  const IS_INFINITE_SCROLL = (isInfiniteScrollEnabled === undefined) ? process.env.NEXT_PUBLIC_ENABLE_INFINITE_SCROLL === 'true' : isInfiniteScrollEnabled
+
+  // Load more button configuration.
+  const loadMoreBtnTitle = () => {
+    let loadMorebtnConfig: any = {
+      title: GENERAL_LOAD_MORE,
+      action: async () => {
+        handlePageChange();
+      },
+      shortMessage: '',
+    }
+    return loadMorebtnConfig
+  }
+
+  const btnConfig = loadMoreBtnTitle()
 
   return (
     <section
@@ -29,18 +50,17 @@ export default function Grid({
         {TITLE_PRODUCTS}
       </h2>
 
-      {IS_INFINITE_SCROLL && (
+      {IS_INFINITE_SCROLL ? (
         <InfiniteScroll
           fetchData={handleInfiniteScroll}
           total={products.total}
           currentNumber={products.results.length}
           component={
             <div
-              className={`border-gray-100 gap-x-8 gap-y-4 grid grid-cols-2 sm:mx-0 md:grid-cols-5 ${
-                products.results.length < 6
-                  ? `lg:grid-cols-5`
-                  : 'lg:grid-cols-5'
-              }`}
+              className={`border-gray-100 gap-x-8 gap-y-4 grid grid-cols-2 sm:mx-0 md:grid-cols-5 ${products.results.length < 6
+                ? `lg:grid-cols-5`
+                : 'lg:grid-cols-5'
+                }`}
             >
               {!products.results.length &&
                 rangeMap(12, (i) => (
@@ -62,36 +82,53 @@ export default function Grid({
             </div>
           }
         />
-      )}
-      {!IS_INFINITE_SCROLL && (
-        <>
-          <div className="border-gray-200 grid grid-cols-2 sm:mx-0 md:grid-cols-4 lg:grid-cols-4">
-            {!products.results.length &&
-              rangeMap(12, (i) => (
-                <div
-                  key={i}
-                  className="shadow-md w-60 h-72 rounded-md mx-auto mt-20"
-                >
-                  <div className="flex animate-pulse flex-row items-center h-full justify-center space-x-5">
-                    <div className="flex flex-col space-y-3">
-                      <div className="w-full bg-gray-100 h-48 rounded-md "></div>
-                      <div className="w-36 bg-gray-100 h-6 mt-40 rounded-md "></div>
+      ) : (
+        products && products?.results ? (
+          <>
+            <div
+              className={`border-gray-100 gap-x-8 gap-y-4 grid grid-cols-2 sm:mx-0 md:grid-cols-5 px-4 sm:px-0 ${products.results.length < 6
+                ? `lg:grid-cols-5`
+                : 'lg:grid-cols-5'
+                }`}
+            >
+              {!products.results.length &&
+                rangeMap(12, (i) => (
+                  <div
+                    key={i}
+                    className="shadow-md w-60 h-72 rounded-md mx-auto mt-20"
+                  >
+                    <div className="flex animate-pulse flex-row items-center h-full justify-center space-x-5">
+                      <div className="flex flex-col space-y-3">
+                        <div className="w-full bg-gray-100 h-48 rounded-md "></div>
+                        <div className="w-36 bg-gray-100 h-6 mt-40 rounded-md "></div>
+                      </div>
                     </div>
                   </div>
-                </div>
+                ))}
+              {products.results.map((product: any, productIdx: number) => (
+                <ProductCard key={productIdx} product={product} />
               ))}
-            {products.results.map((product: any, productIdx: number) => (
-              <ProductCard key={productIdx} product={product} />
-            ))}
-          </div>
-          {products.pages > 1 && (
-            <Pagination
-              currentPage={currentPage}
-              onPageChange={handlePageChange}
-              pageCount={products.pages}
-            />
-          )}
-        </>
+            </div>
+            {products.pages > 1 && !isLastPage && (
+              <div className="w-48 mx-auto sm:my-8 my-6 flex sm:flex-col1">
+                <Button
+                  title={btnConfig.title}
+                  action={btnConfig.action}
+                  buttonType="button"
+                />
+              </div>
+            )}
+            {/*{products.pages > 1 && (
+              <Pagination
+                currentPage={currentPage}
+                onPageChange={handlePageChange}
+                pageCount={products.pages}
+              />
+            )}*/}
+          </>
+        ) : (
+          <></>
+        )
       )}
     </section>
   )
